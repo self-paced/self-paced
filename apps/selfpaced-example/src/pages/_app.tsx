059ae725f5738a1d8ef7d867052005943de8d491
Ravi Souza <ravi.souza@super-studio.jp>
2022-06-01 10:40:13 +0900

    move theme to package

--- apps/selfpaced-example/src/pages/_app.tsx
+++ apps/selfpaced-example/src/pages/_app.tsx
@@ -1,28 +1,16 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { SessionProvider } from 'next-auth/react';
 import { SelfPacedProvider } from 'selfpaced';
 import React from 'react';
 
-const theme = createTheme({
-  palette: {
-    background: {
-      default: '#ECECF4',
-    },
-  },
-});
-
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <SessionProvider session={session}>
-        <SelfPacedProvider>
-          <Component {...pageProps} />
-        </SelfPacedProvider>
-      </SessionProvider>
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <SelfPacedProvider>
+        <Component {...pageProps} />
+      </SelfPacedProvider>
+    </SessionProvider>
   );
 }
 
--- packages/selfpaced/core/components/Provider/SelfPacedProvider.tsx
+++ packages/selfpaced/core/components/Provider/SelfPacedProvider.tsx
@@ -1,6 +1,15 @@
 import { ApolloProvider } from '@apollo/client';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import AuthGuard from '../Auth/AuthGuard';
 import client from '../../api/graphql/client';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#ECECF4',
+    },
+  },
+});
 
 interface Props {
   children?: React.ReactNode;
@@ -8,9 +17,12 @@ interface Props {
 
 const SelfPacedProvider: React.FC<Props> = ({ children }) => {
   return (
-    <ApolloProvider client={client}>
-      <AuthGuard>{children}</AuthGuard>
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ApolloProvider client={client}>
+        <AuthGuard>{children}</AuthGuard>
+      </ApolloProvider>
+    </ThemeProvider>
   );
 };
 
