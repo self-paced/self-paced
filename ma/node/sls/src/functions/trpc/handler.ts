d07ffa0132c92c80945eb7ed272fda85b355bbc6
Ravi Souza <ravi.souza@super-studio.jp>
2022-07-14 11:43:05 +0900

    prepare api project

---
+++ ma/node/sls/src/functions/trpc/handler.ts
@@ -0,0 +1,32 @@
+import * as trpc from '@trpc/server';
+import { CreateAWSLambdaContextOptions, awsLambdaRequestHandler } from '@trpc/server/adapters/aws-lambda';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { z } from 'zod';
+
+export const appRouter = trpc.router().query('hello', {
+  input: z
+    .object({
+      text: z.string().nullish(),
+    })
+    .nullish(),
+  resolve({ input }) {
+    return {
+      greeting: `hello ${input?.text ?? 'world'}`,
+    };
+  },
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+// created for each request
+const createContext = ({
+  // event,
+  // context,
+}: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => ({}) // no context
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+export const main = awsLambdaRequestHandler({
+  router: appRouter,
+  createContext,
+})
