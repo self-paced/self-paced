cd9551b9bbe8c1730716c2e8f1f6b6bb639326b3
Satoru Kikuchi <yahhoi54@gmail.com>
2022-07-19 16:10:56 +0900

    Add albers

--- ma/node/web/src/pages/index.tsx
+++ ma/node/web/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { MouseEventHandler } from 'react';
 import { useDialog } from '../components/AppUtilityProvider/DialogProvider';
 import { trpc } from '../utils/trpc';
 import { useSession, signIn } from 'next-auth/react';
+import { Button } from '@super_studio/ecforce_ui_albers';
 
 const CustomButton: React.FC<{
   onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
@@ -38,6 +39,9 @@ const Home: NextPage = () => {
       <div>
         <div>これはAPIのレスポンスです：</div>
         <pre>{JSON.stringify(users.data, null, 2)}</pre>
+        <p>
+          <Button>success</Button>
+        </p>
         <button
           className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           onClick={async () => {
