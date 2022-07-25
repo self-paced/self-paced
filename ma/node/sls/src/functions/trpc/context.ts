9edd9be4eec740ba9f90c2485417e37fe8fac1ec
Satoru Kikuchi <yahhoi54@gmail.com>
2022-07-25 20:18:26 +0900

    webpackでエラーになるけど、schemaが読めると動く

--- ma/node/sls/src/functions/trpc/context.ts
+++ ma/node/sls/src/functions/trpc/context.ts
@@ -1,8 +1,11 @@
 import { inferAsyncReturnType } from '@trpc/server';
 import { CreateAWSLambdaContextOptions } from '@trpc/server/adapters/aws-lambda';
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { prisma } from '../../db/client';
 
 // 各リクエストの作られます
 export const createContext =
-  ({}: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => ({});
+  ({}: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => {
+    return prisma;
+  };
 export type Context = inferAsyncReturnType<typeof createContext>;
