19ad0e3020677183c44cac032b27751cdab99f4f
Ravi Souza <ravi.souza@super-studio.jp>
2022-07-28 15:17:26 +0900

    apply formatter

--- ma/node/web/src/components/AppUtilityProvider/AppFrame/sidebar.tsx
+++ ma/node/web/src/components/AppUtilityProvider/AppFrame/sidebar.tsx
@@ -1,16 +1,26 @@
 const Sidebar: React.FC = () => {
   return (
-    <aside className="flex w-72 h-full flex-col space-y-2 border-r-2 border-gray-200 bg-white p-2"
-           style={{height: "99vh"}}>
-      <a href="#dashboard"
-         className="flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600">
-        <span className="text-2xl"><i className="bx bx-home"></i></span>
+    <aside
+      className="flex w-72 h-full flex-col space-y-2 border-r-2 border-gray-200 bg-white p-2"
+      style={{ height: '99vh' }}
+    >
+      <a
+        href="#dashboard"
+        className="flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600"
+      >
+        <span className="text-2xl">
+          <i className="bx bx-home"></i>
+        </span>
         <span>Dashboard</span>
       </a>
 
-      <a href="#segment"
-         className="flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600">
-        <span className="text-2xl"><i className="bx bx-cart"></i></span>
+      <a
+        href="#segment"
+        className="flex items-center space-x-1 rounded-md px-2 py-3 hover:bg-gray-100 hover:text-blue-600"
+      >
+        <span className="text-2xl">
+          <i className="bx bx-cart"></i>
+        </span>
         <span>Segment</span>
       </a>
     </aside>
