d883b369e68d8a4c9f47a9d7bba21fa9b497598a
Ravi Souza <ravi.souza@super-studio.jp>
2022-08-09 14:24:37 +0900

    next auth検証

--- ma/node/web/src/pages/api/auth/[...nextauth].tsx
+++ ma/node/web/src/pages/api/auth/[...nextauth].tsx
@@ -1,3 +1,4 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
@@ -58,4 +59,15 @@ export const authOptions: NextAuthOptions = {
   },
 };
 
-export default NextAuth(authOptions);
+const nextAuthHandler = NextAuth(authOptions);
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('NEXT_AUTH');
+  console.log(process.env.NEXTAUTH_URL);
+  console.log(process.env.VERCEL_URL);
+  if (process.env.NEXTAUTH_URL) {
+    req.headers['x-forwarded-host'] = new URL(process.env.NEXTAUTH_URL).host;
+  }
+
+  return nextAuthHandler(req, res);
+}
