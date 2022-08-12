37432488d2c979f12df74675a4d3601e1df66e01
Ravi Souza <ravi.souza@super-studio.jp>
2022-08-12 14:31:40 +0900

    tRPC関連コードをNextJSから削除

--- ma/node/web/src/server/trpc/createRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from './context';
-import * as trpc from '@trpc/server';
-
-/**
- * Context付きのルーターを作るためのヘルパー関数
- */
-export function createRouter() {
-  return trpc.router<Context>();
-}
