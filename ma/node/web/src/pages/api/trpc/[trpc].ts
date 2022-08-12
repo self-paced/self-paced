36bd80f5b2927f1669045fb08659669d54e22995
Ravi Souza <ravi.souza@super-studio.jp>
2022-08-12 14:36:25 +0900

    web側のtRPC設定削除

--- ma/node/web/src/pages/api/trpc/[trpc].ts
+++
@@ -1,9 +0,0 @@
-import { createNextApiHandler } from '@trpc/server/adapters/next';
-import { appRouter } from '../../../server/trpc/routers';
-import { createContext } from '../../../server/trpc/context';
-
-// export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createContext,
-});
