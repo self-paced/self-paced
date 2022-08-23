4d929b9ccbfc10c1a05519f8ca990560387e2e65
Ravi Souza <ravi.souza@super-studio.jp>
2022-08-23 17:13:45 +0900

    cors設定

--- ma/node/sls/src/functions/trpc/handler.ts
+++ ma/node/sls/src/functions/trpc/handler.ts
@@ -4,11 +4,13 @@ import serverlessExpress from '@vendia/serverless-express';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { AppRouter, appRouter } from './routers';
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 
 app.use(
   '/trpc',
