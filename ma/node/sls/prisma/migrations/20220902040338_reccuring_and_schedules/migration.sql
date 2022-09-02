acd33760e39c13082567cf764153e9f33905478f
Satoru Kikuchi <yahhoi54@gmail.com>
2022-09-02 13:38:28 +0900

    seedの変更

---
+++ ma/node/sls/prisma/migrations/20220902040338_reccuring_and_schedules/migration.sql
@@ -0,0 +1,55 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[messageScheduleId]` on the table `MessageEvent` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `messageScheduleId` to the `MessageEvent` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `MessageEvent` ADD COLUMN `messageScheduleId` VARCHAR(191) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `MessageReccuring` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `content` JSON NOT NULL,
+    `deliveryCycle` VARCHAR(191) NOT NULL,
+    `nextDeliveryAt` DATETIME(3) NOT NULL,
+    `startDeliveryTerm` DATETIME(3) NULL,
+    `endDeliveryTerm` DATETIME(3) NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `projectId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MessageSchedule` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `content` JSON NOT NULL,
+    `deliveryScheduleAt` DATETIME(3) NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `status` VARCHAR(191) NOT NULL DEFAULT 'draft',
+    `projectId` VARCHAR(191) NOT NULL,
+    `messageReccuringId` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `MessageEvent_messageScheduleId_key` ON `MessageEvent`(`messageScheduleId`);
+
+-- AddForeignKey
+ALTER TABLE `MessageReccuring` ADD CONSTRAINT `MessageReccuring_projectId_fkey` FOREIGN KEY (`projectId`) REFERENCES `Account`(`projectId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MessageSchedule` ADD CONSTRAINT `MessageSchedule_projectId_fkey` FOREIGN KEY (`projectId`) REFERENCES `Account`(`projectId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MessageSchedule` ADD CONSTRAINT `MessageSchedule_messageReccuringId_fkey` FOREIGN KEY (`messageReccuringId`) REFERENCES `MessageReccuring`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MessageEvent` ADD CONSTRAINT `MessageEvent_messageScheduleId_fkey` FOREIGN KEY (`messageScheduleId`) REFERENCES `MessageSchedule`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- ma/node/sls/prisma/seed.ts
+++ ma/node/sls/prisma/seed.ts
@@ -1,14 +1,50 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 (async () => {
   console.log('Seeding...');
   await ['local', 'demo35', 'futsunoshop'].map(async (projectId) => {
-    await prisma.account.create({
-      data: {
-        projectId,
+    await prisma.account.upsert({
+      where: {
+        projectId: projectId,
+      },
+      update: {},
+      create: {
+        projectId: projectId,
       },
     });
   });
+
+  const emptyMessageScheduleIds = await prisma.messageEvent.findMany({
+    where: {
+      messageScheduleId: undefined,
+    },
+  });
+
+  emptyMessageScheduleIds.map(async (messageEvent) => {
+    const messageSchedule = await prisma.messageSchedule.create({
+      data: {
+        title: messageEvent.title,
+        content: messageEvent.content as Prisma.JsonObject,
+        deliveryScheduleAt: messageEvent.createdAt,
+        status: 'done',
+        account: {
+          connect: {
+            projectId: 'demo35', // todo こちら一時的なので決め打ち
+          },
+        },
+      },
+    });
+
+    await prisma.messageEvent.update({
+      where: {
+        id: messageEvent.id,
+      },
+      data: {
+        messageScheduleId: messageSchedule.id,
+      },
+    });
+  });
+
   console.log('Seed Complete!');
 })();
